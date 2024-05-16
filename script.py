70b91e948a5a27a975c98fee5fe9846f7861f4ba
nicolenaspect <nicolepavlova9@gmail.com>
2024-05-16 19:19:52 +0300

    Fixes

--- script.py
+++ script.py
@@ -6,51 +6,51 @@ def ui_controller():
     print('5. Hollow Square')
     print('6. Pyramid')
     print('7. Cancel')
-    user_choice = int(input('Choose your Option: '))
-    if user_choice == 7:
+    user_choice_input = int(input('Choose your Option: '))
+    if user_choice_input == 7:
         exit()
-    user_size = int(input('Choose your size: '))
-    return user_choice, user_size
+    user_size_input = int(input('Choose your size: '))
+    return user_choice_input, user_size_input
 
 
-pattern_choice, urs_size = ui_controller()
+pattern_choice, user_size = ui_controller()
 print("Pattern choice:", pattern_choice)
-print("Size:", urs_size)
+print("Size:", user_size)
 
 
-def solver(choice, usr_size):
+def solver(choice, size):
     while True:
         match choice:
             case 1:
-                for i in range(1, usr_size + 1):
+                for i in range(1, size + 1):
                     print('*' * i)
             case 2:
-                for i in range(usr_size):
-                    for j in range(usr_size):
-                        if i == 0 or i == usr_size - 1 or j == 0 or j == usr_size - 1:
+                for i in range(size):
+                    for j in range(size):
+                        if i == 0 or i == size - 1 or j == 0 or j == size - 1:
                             print("*", end="")
                         else:
                             print(" ", end="")
                     print()
             case 3:
-                for i in range(1, usr_size + 1, 2):
-                    print(" " * ((usr_size - i) // 2) + "*" * i + " " * ((usr_size - i) // 2))
-                for i in range(usr_size - 2, 0, -2):
-                    print(" " * ((usr_size - i) // 2) + "*" * i + " " * ((usr_size - i) // 2))
+                for i in range(1, size + 1, 2):
+                    print(" " * ((size - i) // 2) + "*" * i + " " * ((size - i) // 2))
+                for i in range(size - 2, 0, -2):
+                    print(" " * ((size - i) // 2) + "*" * i + " " * ((size - i) // 2))
             case 4:
-                for i in range(usr_size, 0, -1):
+                for i in range(size, 0, -1):
                     print('*' * i)
             case 5:
-                for i in range(usr_size):
-                    if i == 0 or i == usr_size - 1:
-                        print("*" * usr_size)
+                for i in range(size):
+                    if i == 0 or i == size - 1:
+                        print("*" * size)
                     else:
-                        print("*" + " " * (usr_size - 2) + "*")
+                        print("*" + " " * (size - 2) + "*")
             case 6:
-                for i in range(1, usr_size + 1):
-                    print(" " * (usr_size - i), end="")
+                for i in range(1, size + 1):
+                    print(" " * (size - i), end="")
                     print("*" * (2 * i - 1))
         ui_controller()
 
 
-solver(pattern_choice, urs_size)
+solver(pattern_choice, user_size)
