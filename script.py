6dc4cf9f379ec376b3b7e2c1c77a40f59c01758f
nicolenaspect <nicolepavlova9@gmail.com>
2024-05-16 19:00:36 +0300

    Added left-angled Triangle and UI Changes

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 def ui_controller():
+
     print('1. Right-angled Triangle')
     print('2. Square with Hollow Center')
     print('3. Diamond')
     print('4. Left-angled Triangle')
     print('5. Hollow Square')
-
-    user_choice = int(input('Choose your pattern: '))
+    print('6. Cancel')
+    user_choice = int(input('Choose your Option: '))
+    if user_choice == 6:
+        exit()
     user_size = int(input('Choose your size: '))
-
     return user_choice, user_size
 
 
@@ -35,12 +37,14 @@ def solver(choice, usr_size):
                     print(" " * ((usr_size - i) // 2) + "*" * i + " " * ((usr_size - i) // 2))
                 for i in range(usr_size - 2, 0, -2):
                     print(" " * ((usr_size - i) // 2) + "*" * i + " " * ((usr_size - i) // 2))
-
             case 4:
-                pass
+                for i in range(usr_size, 0, -1):
+                    print('*' * i)
             case 5:
                 pass
-        break
+            case 6:
+                break
+        ui_controller()
 
 
 solver(pattern_choice, size)
