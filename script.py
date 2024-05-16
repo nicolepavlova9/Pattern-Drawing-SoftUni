2cdada3102026c3e618729e49d4b4785b89a6baa
nicolenaspect <nicolepavlova9@gmail.com>
2024-05-16 19:12:05 +0300

    Added hollow square and pyramid

--- script.py
+++ script.py
@@ -1,21 +1,21 @@
 def ui_controller():
-
     print('1. Right-angled Triangle')
     print('2. Square with Hollow Center')
     print('3. Diamond')
     print('4. Left-angled Triangle')
     print('5. Hollow Square')
-    print('6. Cancel')
+    print('6. Pyramid')
+    print('7. Cancel')
     user_choice = int(input('Choose your Option: '))
-    if user_choice == 6:
+    if user_choice == 7:
         exit()
     user_size = int(input('Choose your size: '))
     return user_choice, user_size
 
 
-pattern_choice, size = ui_controller()
+pattern_choice, urs_size = ui_controller()
 print("Pattern choice:", pattern_choice)
-print("Size:", size)
+print("Size:", urs_size)
 
 
 def solver(choice, usr_size):
@@ -41,10 +41,16 @@ def solver(choice, usr_size):
                 for i in range(usr_size, 0, -1):
                     print('*' * i)
             case 5:
-                pass
+                for i in range(usr_size):
+                    if i == 0 or i == usr_size - 1:
+                        print("*" * usr_size)
+                    else:
+                        print("*" + " " * (usr_size - 2) + "*")
             case 6:
-                break
+                for i in range(1, usr_size + 1):
+                    print(" " * (usr_size - i), end="")
+                    print("*" * (2 * i - 1))
         ui_controller()
 
 
-solver(pattern_choice, size)
+solver(pattern_choice, urs_size)
